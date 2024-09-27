45cf2668fed11d08f45b1fc5950385b5b9b208d3
Ana Ruy <anaflavia.ruy@nibo.com.br>
2024-09-26 21:19:01 -0300

    adição do conteúdo da aula sobre json, como percorrer entre os objetos

--- Aulas/JSON/index.js
+++ Aulas/JSON/index.js
@@ -19,6 +19,7 @@ generateInvoice(name, products, productsValues, age);
 */
 
 // COM JSON
+/*
 let invoice = {
     name: "Ana",
     age: 25,
@@ -44,4 +45,33 @@ function generateInvoice(invoiceProducts) {
     console.log("O valor é " + invoiceProducts.products[2][1]);   // Valor do produto
 }
 
+*/
 
+// PERCORRER NOS OBJETOS
+let invoice = {
+    name: "Ana",
+    age: 25,
+    products: {
+        0: ["Mouse", 29.99],
+        1: ["Teclado", 129.99],
+        2: ["Monitor", 829.99]
+    },
+    productsValues: [29.99, 129.99, 899.99]
+};
+
+// Chama a função
+generateInvoice(invoice);
+
+function generateInvoice(invoice) {
+    // Acesse as propriedades do objeto invoice
+    console.log(`O comprador é ${invoice.name}`);
+    console.log(`A idade é ${invoice.age}`);
+    console.log("----------------");
+    
+    // Loop pelos produtos
+    for (let index in invoice.products) {
+        // Acessa nome e preço do produto
+        let [productName, productPrice] = invoice.products[index];
+        console.log(`Produto: ${productName}, Preço: R$${productPrice.toFixed(2)}`);
+    }
+}
